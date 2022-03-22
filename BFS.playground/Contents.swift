7883d4d0c5d841f8db0270555bc50eb3455d39f2
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-03-22 17:32:44 +0100

    cleanup

--- BFS.playground/Contents.swift
+++ BFS.playground/Contents.swift
@@ -3,8 +3,8 @@ import Foundation
 
 class Node<T> {
     var value: T
-    var left: Node<T>? = nil
-    var right: Node<T>? = nil
+    var left: Node<T>?
+    var right: Node<T>?
     
     init(value: T) {
         self.value = value
